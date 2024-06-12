34d9d0436bb461387ea0fa8d1251315391160677
YuxiaoLi1234 <li.14025@osu.edu>
2024-06-12 11:20:38 +0800

    zfp

--- runomp.sh
+++ runomp.sh
@@ -13,7 +13,7 @@ module load parallel
 
 
 # parallel -j4 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); ./helloworld {1} {2} {3} 0;' ::: "finger,128,128,128" :::: values.txt ::: sz3 zfp
-parallel -j4 './helloworldomp {1} {2} {3} 0 {4};' ::: "finger,128,128,128" :::: bounds.txt ::: sz3 :::: values.txt
+parallel -j4 './helloworldomp {1} {2} {3} 0 {4};' ::: "finger,128,128,128" :::: bounds.txt ::: sz3 zfp :::: values.txt
 # parallel -j2 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); ./helloworld {1} {2} {3};' ::: "finger,128,128,128" "Red_sea,500,500,50" "earthquake,750,375,100" "NYX,512,512,512" "S3D,500,500,500" ::: $(awk 'BEGIN{for(i=0;i<100;i++) print 1e-6 + i*(1e-5-1e-6)/99}') ::: sz3 zfp
 
 # parallel -j4 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); num=$(echo {1} | awk -F,"'"'{print $NF}'"'"'); if [ "$num" -eq 1 ]; then ./helloworld {1} {2} {3}; else ./helloworld2 {1} {2} {3}; fi' ::: "NYX,512,512,512,0" ::: 1e-6 1e-5 1e-4 1e-3 1e-2 1e-1 ::: sz3 zfp
