cb7812b486c44c83a8523cbd12782079ef1c5637
YuxiaoLi1234 <li.14025@osu.edu>
2024-06-13 14:39:22 +0800

    osc.sh

---
+++ run_omp_osc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=my_job
+#SBATCH --nodes=1
+#SBATCH --time=02:00:00
+#SBATCH --qos=normal
+#SBATCH --constraint=gpu
+#SBATCH --account=PAS2402
+
+
+module load cuda
+
+
+parallel -j1 './helloworldomp {1} {2} {3} 0 {4};' ::: "finger,128,128,128" :::: bounds.txt ::: sz3 zfp :::: values.txt
