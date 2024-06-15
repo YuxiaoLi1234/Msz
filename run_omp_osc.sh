685fab1f9240f78fe24bbf9ba47f687581dd1808
YuxiaoLi1234 <li.14025@osu.edu>
2024-06-15 15:58:16 +0800

    .sh

--- run_omp_osc.sh
+++ run_omp_osc.sh
@@ -6,10 +6,10 @@
 #SBATCH --constraint=gpu
 #SBATCH --account=PAS2402
 
-
+values = (5e-6,1e-5,5e-5,1e-4,5e-4,1e-3,5e-3,1e-2,5e-2)
 module load cuda
-
-./helloworldomp finger,128,128,128 1e-6 sz3 0 32
-./helloworldomp_d finger,128,128,128 1e-6 sz3 0 32
-./helloworld_determin finger,128,128,128 1e-6 sz3 0
-./helloworld finger,128,128,128 1e-6 sz3 0
+for i in "${values[@]}":
+    ./helloworldomp finger,128,128,128 $i sz3 0 32
+    ./helloworldomp_d finger,128,128,128 $i sz3 0 32
+    ./helloworld_determin finger,128,128,128 $i sz3 0
+    ./helloworld finger,128,128,128 $i sz3 0
