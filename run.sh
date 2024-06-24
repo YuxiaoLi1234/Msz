8b1b99a7efb056e31620ed9fe7011aea676211f8
YuxiaoLi1234 <li.14025@osu.edu>
2024-06-24 23:43:11 +0800

    e

--- run.sh
+++ run.sh
@@ -15,8 +15,8 @@ nvcc -c kernel3d.cu -o kernel.o
 g++-12 -std=c++17 -O3 -g -fopenmp -c preserve3d.cpp -o hello2.o
 g++-12 -fopenmp hello2.o kernel.o -L/opt/nvidia/hpc_sdk/Linux_x86_64/23.9/cuda/12.2/lib64 -lcudart -o helloworld
 
-parallel -j1 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); ./helloworld {1} {2} {3} 0;' ::: "finger,128,128,128" "Red_sea,500,500,50" "earthquake,750,375,100" "NYX,512,512,512" "at,177,95,48" "heated,150,450,1" "CSEM,1800,3600,1"  :::: bounds.txt ::: sz3 zfp
-parallel -j1 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); ./helloworld {1} {2} {3} 0;' ::: "ivt,576,361,1" "jet_cropped,100,100,100" "nyx2,50,50,50" ::: 1e-2 ::: sz3 zfp
+parallel -j1 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); ./helloworld {1} {2} {3} 0;' ::: "at,177,95,48" "heated,150,450,1" "CSEM,1800,3600,1"  :::: bounds.txt ::: sz3 zfp
+parallel -j1 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); ./helloworld {1} {2} {3} 0;' ::: "jet_cropped,100,100,100" ::: 1e-2 ::: sz3 zfp
 parallel -j1 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); ./helloworld {1} {2} {3} 0;' ::: "at,177,95,48" ::: 1e-3 ::: sz3 zfp
 # parallel -j4 './helloworldmp {1} {2} {3} 0 {4};' ::: "finger,128,128,128" :::: values.txt ::: sz3
 # parallel -j2 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); ./helloworld {1} {2} {3};' ::: "finger,128,128,128" "Red_sea,500,500,50" "earthquake,750,375,100" "NYX,512,512,512" "S3D,500,500,500" ::: $(awk 'BEGIN{for(i=0;i<100;i++) print 1e-6 + i*(1e-5-1e-6)/99}') ::: sz3 zfp
