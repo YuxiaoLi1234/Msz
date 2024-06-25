ba9364e97352274804126a8a1bced3d1d5d37e31
YuxiaoLi1234 <li.14025@osu.edu>
2024-06-25 10:09:10 +0800

    e

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ nvcc -c kernel3d.cu -o kernel.o
 g++-12 -std=c++17 -O3 -g -fopenmp -c preserve3d.cpp -o hello2.o
 g++-12 -fopenmp hello2.o kernel.o -L/opt/nvidia/hpc_sdk/Linux_x86_64/23.9/cuda/12.2/lib64 -lcudart -o helloworld
 
-parallel -j1 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); ./helloworld {1} {2} {3} 0;' ::: "at,177,95,48" "heated,150,450,1" "CSEM,1800,3600,1"  :::: bounds.txt ::: sz3 zfp
+parallel -j1 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); ./helloworld {1} {2} {3} 0;' ::: "CSEM,1800,3600,1"  ::: 5e-6 1e-5 ::: sz3 zfp
 parallel -j1 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); ./helloworld {1} {2} {3} 0;' ::: "jet_cropped,100,100,100" ::: 1e-2 ::: sz3 zfp
 parallel -j1 'export CUDA_VISIBLE_DEVICE=$(({%}-1)); ./helloworld {1} {2} {3} 0;' ::: "at,177,95,48" ::: 1e-3 ::: sz3 zfp
 # parallel -j4 './helloworldmp {1} {2} {3} 0 {4};' ::: "finger,128,128,128" :::: values.txt ::: sz3
